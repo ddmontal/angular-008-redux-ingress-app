f312e4ef678d27e4f2a5d5c7267d7ad00da970d8
David Montalvillo <david@Davids-MacBook-Pro.local>
2019-10-28 19:04:02 +0100

    feat: Add SweetAlert package

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -16,18 +17,19 @@ export class AuthService {
         this.router.navigate(['/']);
       })
       .catch(err => {
-        console.error(err);
+        Swal.fire('Error', err.message, 'error');
       });
-  }
+    }
 
-  login(email: string, password: string) {
+    login(email: string, password: string) {
     this.afAuth.auth
-      .signInWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log(res);
+    .signInWithEmailAndPassword(email, password)
+    .then(res => {
+      console.log(res);
+      this.router.navigate(['/']);
       })
       .catch(err => {
-        console.error(err);
+        Swal.fire('Error', err.message, 'error');
       });
   }
 }
