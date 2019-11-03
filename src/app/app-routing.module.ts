b22b7cbb8b62651f411fd544df7fa892e876a37c
David Montalvillo <david@Davids-MacBook-Pro.local>
2019-11-03 09:43:19 +0100

    feat: Prevent loading module when user is not authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { AuthGuardService } from './auth/auth-guard.service';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', loadChildren: './entry/entry.module#EntryModule'},
+  { path: '', loadChildren: './entry/entry.module#EntryModule', canLoad: [ AuthGuardService ]},
   { path: '**', redirectTo: '' }
 ];
 
